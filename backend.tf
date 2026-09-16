0643fac0f29863c36185b4be220ab36ce316abff
Ajay Chikte <your-github-email@example.com>
2026-09-16 12:50:30 +0530

    bas kr bhai

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "my-bucket"
+    bucket       = "terraform-bucket13"
     key          = "projects/myapp2/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
